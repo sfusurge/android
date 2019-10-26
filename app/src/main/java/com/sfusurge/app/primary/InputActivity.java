d3f8793e7db6b9114239816d2a83cd820fc5f295
Jeffrey Leung <jleung513@gmail.com>
2019-10-26 02:28:09 -0700

    Save and skip re-entering preferences

--- app/src/main/java/com/sfusurge/app/primary/InputActivity.java
+++ app/src/main/java/com/sfusurge/app/primary/InputActivity.java
@@ -3,46 +3,66 @@ package com.sfusurge.app.primary;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.sfusurge.app.primary.service.PreferencesManager;
 import com.sfusurge.app.primary.service.TextAndViewManager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-import android.view.WindowManager;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputActivity extends AppCompatActivity {
 
     private static final int[] inputFieldResourceIds = {
             R.id.first_name_input,
             R.id.last_name_input,
-            R.id.sfu_email_input
+            R.id.email_input
     };
+
+    private static final String fieldsSet = "fieldsSet";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
 
-        PreferencesManager.setDefaults(this, R.xml.root_preferences);
+        // Skip if already filled in
+        if (fieldsSet()) {
+            navigateToNextActivity();
+        }
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        // Link button to save input
         FloatingActionButton button = findViewById(R.id.set_input_details);
-
         final Activity context = this;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (inputFieldsCompleted()) {
-                    startActivity(new Intent(InputActivity.this, ScanActivity.class));
-                } else {
+                if (!inputFieldsCompleted()) {
                     TextAndViewManager.setVisible(context, R.id.input_warning);
+                } else {
+                    PreferencesManager.setStrings(context, getInputFields());
+                    PreferencesManager.setBoolean(context, fieldsSet, true);
+                    navigateToNextActivity();
                 }
             }
         });
+    }
+
+    // Field is set by button when advancing
+    private boolean fieldsSet() {
+        return PreferencesManager.getBoolean(this, fieldsSet);
+    }
+
+    private void navigateToNextActivity() {
+        startActivity(new Intent(InputActivity.this, ScanActivity.class));
+        finishActivity(0);
     }
 
     private boolean inputFieldsCompleted() {
@@ -52,6 +72,26 @@ public class InputActivity extends AppCompatActivity {
             }
         }
         return true;
+    }
+
+    private List<Pair<String, String>> getInputFields() {
+        List<Pair<String, String>> list = new ArrayList<>();
+
+        // Set preferences in root_preferences.xml
+        list.add(new Pair<>(
+                "first_name",
+                TextAndViewManager.getValueOfInputField(this, R.id.first_name_input)
+        ));
+        list.add(new Pair<>(
+                "last_name",
+                TextAndViewManager.getValueOfInputField(this, R.id.last_name_input)
+        ));
+        list.add(new Pair<>(
+                "email",
+                TextAndViewManager.getValueOfInputField(this, R.id.email_input)
+        ));
+
+        return list;
     }
 
 }
--- app/src/main/java/com/sfusurge/app/primary/service/PreferencesManager.java
+++ app/src/main/java/com/sfusurge/app/primary/service/PreferencesManager.java
@@ -3,18 +3,74 @@ package com.sfusurge.app.primary.service;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @SuppressWarnings("unused")
 public class PreferencesManager {
 
+    /**
+     * Sets the pre-defined default values for the Preferences in a resource file.
+     *
+     * @see androidx.preference.PreferenceScreen
+     *
+     * @param context Context of the calling Activity.
+     * @param resourceId Resource of the preferences which include default values to set.
+     */
     public static void setDefaults(Activity context, int resourceId) {
         PreferenceManager.setDefaultValues(context, resourceId, false);
     }
 
+    /**
+     * Retrieves a string value from Preferences.
+     *
+     * @param context Context of the calling Activity.
+     * @param key Identifier of the value to return.
+     * @return The Preferences value, or an empty string if none exists.
+     */
     public static String getString(Activity context, String key) {
-        SharedPreferences sharedPreferences =
-                PreferenceManager.getDefaultSharedPreferences(context);
-        return sharedPreferences.getString(key, "");
+        return PreferenceManager
+                .getDefaultSharedPreferences(context)
+                .getString(key, "");
+    }
+
+    public static void setString(Activity context, String key, String value) {
+        setStrings(context, Collections.singletonList(new Pair<>(key, value)));
+    }
+
+    public static void setStrings(Activity context, List<Pair<String, String>> keys_values) {
+        SharedPreferences.Editor prefs = getEditableSharedPreferences(context);
+        for (Pair<String, String> kv : keys_values) {
+            prefs.putString(kv.first, kv.second);
+        }
+        prefs.apply();
+    }
+
+    /**
+     * Retrieves a boolean value from Preferences.
+     *
+     * @param context Context of the calling Activity.
+     * @param key Identifier of the value to return.
+     * @return The Preferences value, or false if none exists.
+     */
+    public static boolean getBoolean(Activity context, String key) {
+        return PreferenceManager
+                .getDefaultSharedPreferences(context)
+                .getBoolean(key, false);
+    }
+
+    public static void setBoolean(Activity context, String key, boolean b) {
+        getEditableSharedPreferences(context)
+                .putBoolean(key, b)
+                .apply();
+    }
+
+    private static SharedPreferences.Editor getEditableSharedPreferences(Activity context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).edit();
     }
 
 }
